ed6d5b1a0c482016b0c5f9fcfe7c77d90e83b0dc
Danilo Téo <danilo.teo@liven.tech>
2020-10-25 15:27:51 -0300

    screen for a new tweet

--- screens/NewTweetScreen/index.tsx
+++ screens/NewTweetScreen/index.tsx
@@ -1,17 +1,44 @@
 import React from 'react'
-import {Container,PostTweetButton,TopElements,StyledText } from  './styles'
+import {Container,PostTweetButton,TopElements,StyledText,MiddleContainer, InputArea, TweetInput, ImageInput } from  './styles'
 import {AntDesign} from '@expo/vector-icons'
 import Colors from '../../constants/Colors'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import ProfilePicture from '../../components/ProfilePicture'
 const NewTweetScreen = () =>{
+    const [tweet, setTweet] = React.useState('')
+    const [imageUrl, setImageUrl] = React.useState('')
+    
+    const onPostTweet = () => {
+        console.log('Posting tweet: ', tweet)
+        console.log('image url: ', imageUrl)
+    }
+
+
     return(
         <Container>
             <TopElements>
                 <AntDesign name="close" size={30} color={Colors.light.tint}/>
-                <PostTweetButton>
+                <PostTweetButton onPress={onPostTweet}>
                     <StyledText>Tweet</StyledText>
                 </PostTweetButton>
             </TopElements>
+                <MiddleContainer>
+                    <ProfilePicture image={'https://pbs.twimg.com/profile_images/1278506514119036929/SPYmN0ls_200x200.jpg'}/>
+                   <InputArea>
+                    <TweetInput
+                        value={tweet}
+                        numberOfLines={3}
+                        multiline={true}
+                        placeholder={'What\'s happening?'}
+                        onChangeText={(input) =>  setTweet(input)}
+                        
+                    />
+                    <ImageInput
+                        value={imageUrl}
+                        placeholder={'Image url (optional)'}
+                        onChangeText={(input) =>  setImageUrl(input)}
+                    />
+                    </InputArea>
+                </MiddleContainer>
         </Container>
 
     )
--- screens/NewTweetScreen/styles.tsx
+++ screens/NewTweetScreen/styles.tsx
@@ -1,18 +1,46 @@
 import styled from 'styled-components/native'
 import Colors from '../../constants/Colors'
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
     flex: 1;
-    align-items: center;
-    justify-content:center;
+    align-items: flex-start;
+    background-color: white;
 `
 
 export const TopElements = styled.View`
+    width: 100%;    
     flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    padding-top: 20px;
 `
 
 export const PostTweetButton = styled.TouchableOpacity`
     background-color: ${Colors.light.tint}
+    border-radius: 30px
 `
 
-export const StyledText = styled.Text``
+export const StyledText = styled.Text`
+    padding-horizontal: 20px;
+    padding-vertical: 10px;
+    color: white;
+    font-weight: bold;
+    font-size: 16px;
+`
+
+export const MiddleContainer = styled.View`
+    flex-direction:row;
+    padding: 15px;
+
+`
+
+export const InputArea = styled.View`
+    margin-left:10px;
+`
+export const TweetInput = styled.TextInput`
+    height: 100px;
+    max-height: 300px;
+    font-size:20px
+`
+export const ImageInput = styled.TextInput``
